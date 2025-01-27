03888c5b3880f671a6a7d2718799e966c636f89e
Santyls <123048209@upq.edu.mx>
2025-01-27 15:39:09 -0600

    se concluyo la actividad no3

--- PNO3/src/Main.java
+++ PNO3/src/Main.java
@@ -1,10 +1,23 @@
 import javax.swing.*;
 public class Main {
-//    public static String capitalize(String inputString){
-//        char firstLetter = inputString.charAt(0);
-//        char capitalFirstLetter = Character.toUpperCase(firstLetter);
-//        return inputString.replace(inputString.charAt(0),capitalFirstLetter);
-//    }
+    public static String capitalize(String inputString){
+        char firstLetter = inputString.charAt(0);
+        char capitalFirstLetter = Character.toUpperCase(firstLetter);
+        return inputString.replace(inputString.charAt(0),capitalFirstLetter);
+    }
+public static String reverseInputString(String myString) {
+
+    if (myString == null)
+        return myString;
+
+    String reverseString = "";
+
+    for (int i = myString.length() - 1; i >= 0; i--) {
+
+        reverseString = reverseString + myString.charAt(i);
+    }
+    return reverseString;
+}
     public static void main(String[] args) {
         //1
 //        double horas, costo, res;
@@ -14,17 +27,17 @@ public class Main {
 //        JOptionPane.showMessageDialog(null,"Su salario es de: "+res+"$");
 
         //2
-//        String nombre;
-//        String apellidoPa;
-//        String apellidoMa;
-//
-//        nombre = JOptionPane.showInputDialog(null,"Ingrese su nombre");
-//        apellidoPa = JOptionPane.showInputDialog(null,"Ingrese su apellido paterno");
-//        apellidoMa = JOptionPane.showInputDialog(null,"Ingrese su apellido materno");
-//
-//        JOptionPane.showMessageDialog(null,nombre.toLowerCase()+" "+apellidoPa.toLowerCase()+" "+apellidoMa.toLowerCase()+"\n "
-//                +nombre.toUpperCase()+" "+apellidoPa.toUpperCase()+" "+apellidoMa.toUpperCase()+"\n "
-//                +capitalize(nombre.toLowerCase())+" "+capitalize(apellidoPa.toLowerCase())+" "+capitalize(apellidoMa.toLowerCase()));
+        String nombre;
+        String apellidoPa;
+        String apellidoMa;
+
+        nombre = JOptionPane.showInputDialog(null,"Ingrese su nombre");
+        apellidoPa = JOptionPane.showInputDialog(null,"Ingrese su apellido paterno");
+        apellidoMa = JOptionPane.showInputDialog(null,"Ingrese su apellido materno");
+
+        JOptionPane.showMessageDialog(null,nombre.toLowerCase()+" "+apellidoPa.toLowerCase()+" "+apellidoMa.toLowerCase()+"\n "
+                +nombre.toUpperCase()+" "+apellidoPa.toUpperCase()+" "+apellidoMa.toUpperCase()+"\n "
+                +capitalize(nombre.toLowerCase())+" "+capitalize(apellidoPa.toLowerCase())+" "+capitalize(apellidoMa.toLowerCase()));
           //3
 //        int x,suma = 0;
 //        x = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese un número"));
@@ -33,8 +46,11 @@ public class Main {
 //        }
 //        JOptionPane.showMessageDialog(null,"La suma de todos los enteros del 0 al "+x+" es: "+suma);
 
-        //4
-        String nombre;
+//        //4
+//        String nombre;
+//
+//        nombre = JOptionPane.showInputDialog(null,"Ingrese su nombre");
+//        JOptionPane.showMessageDialog(null,"<"+nombre.toUpperCase()+"> tiene <"+nombre.length()+"> letras" );
 
         //5 Payasos y muñecas
 //        int payasos = 112;
