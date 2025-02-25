6ea3e3bfa77defa8c90f64a7f3dbcc544c710666
Santyls <123048209@upq.edu.mx>
2025-02-25 16:05:44 -0600

    se modifico la clase Spartan y se volvieron privados los atributos, ahora los objetos se instancian atraves del metodo constructor. y se creo un metodo privado, consultaCortana que solo la clase Spartan puede usar.

--- Halo/src/Main.java
+++ Halo/src/Main.java
@@ -3,14 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         //creamos Objeto = Instanciar objeto
-        Spartan jefeMaestro = new Spartan();
-        Spartan reach = new Spartan();
-
-        //asignamos los atributos del objeto
-        jefeMaestro.nombre= "John 117";
-        jefeMaestro.salud= 100;
-        jefeMaestro.escudo= 90;
-        jefeMaestro.armaPrincipal= "Rifle de asalto";
+        Spartan jefeMaestro = new Spartan("John 117",100,90,"Rifle de asalto");
+        Spartan reach = new Spartan("Emilie", 0, 0, "Aguijoneador");
 
         //Usar sus metodos
         jefeMaestro.mostrarInfo();
@@ -19,15 +13,15 @@ public class Main {
         jefeMaestro.correr(true);
 
 
-        //Segundo objeto reach
-        reach.nombre= "Emile";
-        reach.salud= 0;
-        reach.escudo= 0;
-        reach.armaPrincipal= "Aguijoneador";
-
         reach.mostrarInfo();
         reach.atacar("Grunt");
         reach.recargarArma(0);
         reach.correr(false);
+
+        jefeMaestro.setNombre("Ivan Isay");
+        jefeMaestro.mostrarInfo();
+        System.out.println(jefeMaestro.getNombre());
+
+        jefeMaestro.consultaCortana();
     }
 }//llave de la clase main
--- Halo/src/Spartan.java
+++ Halo/src/Spartan.java
@@ -1,9 +1,50 @@
 public class Spartan {
 
-    public String nombre;
-    int salud;
-    int escudo;
-    String armaPrincipal;
+    private String nombre;
+
+    public Spartan(String nombre, int salud, int escudo, String armaPrincipal) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.escudo = escudo;
+        this.armaPrincipal = armaPrincipal;
+    }
+
+    private int salud;
+    private int escudo;
+    private String armaPrincipal;
+
+    //Zona de GET y SET
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getEscudo() {
+        return escudo;
+    }
+
+    public void setEscudo(int escudo) {
+        this.escudo = escudo;
+    }
+
+    public String getArmaPrincipal() {
+        return armaPrincipal;
+    }
+
+    public void setArmaPrincipal(String armaPrincipal) {
+        this.armaPrincipal = armaPrincipal;
+    }
 
     public void mostrarInfo(){
         System.out.println("---------Información del Spartan---------");
@@ -30,6 +71,10 @@ public class Spartan {
         }else{
             System.out.println("El Spartan "+nombre+" se detuvo");
         }
+    }
+
+    private void consultaCortana(){
+        System.out.println("Conversación Privada...");
     }
 
 }//llave de la clase
