63bdc1a26f0aa80dacb2465730b37e42eb7d88ab
Santyls <123048209@upq.edu.mx>
2025-03-26 16:40:42 -0600

    Practicas forms
    Practicas Forms, insert, select y update listos y funcionando

---
+++ CRUDUser204/src/FormRegistro.java
@@ -0,0 +1,223 @@
+
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
+
+public class FormRegistro extends javax.swing.JFrame {
+
+    
+    public FormRegistro() {
+        initComponents();
+        verificarConexion();
+    }
+    
+    private void verificarConexion(){
+        Connection conex = ConexionMySQL.conectar();
+        
+        if(conex != null){
+            JOptionPane.showMessageDialog(this,"Conectado con Exito","Conexión",JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(this,"Error de conexión","Conexión",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        txtCorreo = new javax.swing.JTextField();
+        txtContrasena = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        btnGuardar = new javax.swing.JButton();
+        btnLimpiar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI Semibold", 1, 18)); // NOI18N
+        jLabel1.setText("Registro Usuarios");
+
+        txtNombre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNombreActionPerformed(evt);
+            }
+        });
+
+        txtContrasena.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtContrasenaActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Nombre:");
+
+        jLabel3.setText("Correo:");
+
+        jLabel4.setText("Contraseña:");
+
+        btnGuardar.setText("Guardar");
+        btnGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarActionPerformed(evt);
+            }
+        });
+
+        btnLimpiar.setText("Limpiar");
+        btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(87, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(136, 136, 136))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(115, 115, 115)
+                            .addComponent(btnGuardar)
+                            .addGap(18, 18, 18)
+                            .addComponent(btnLimpiar))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(71, 71, 71)
+                            .addComponent(jLabel4)
+                            .addGap(18, 18, 18)
+                            .addComponent(txtContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addComponent(jLabel1)
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnGuardar)
+                    .addComponent(btnLimpiar))
+                .addGap(48, 48, 48))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        
+        //1. Guardamos en variables lo que se escribe el usuario
+        String nom = txtNombre.getText();
+        String cor = txtCorreo.getText();
+        String con = txtContrasena.getText();
+        
+        //2. validar que no existan vacios
+        if(nom.isEmpty() || cor.isEmpty() || con.isEmpty()){
+            JOptionPane.showMessageDialog(this,"Todos los campos son obligatorios","Error en el formulario",JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        //3. Procedemos a INSERTAR
+        UserCRUD crud = new UserCRUD();
+        boolean status = crud.insertUsuario(nom, cor, con);
+        
+        //4. Avisamos el status al Usuario del INSERT
+        if(status){
+            JOptionPane.showMessageDialog(this,"Usuario guardado con exito en base de datos","Registro exitoso",JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(this,"No se guardo","Error",JOptionPane.ERROR_MESSAGE);
+        }
+        limpiarcampos();
+    }//GEN-LAST:event_btnGuardarActionPerformed
+
+    private void limpiarcampos(){
+        txtNombre.setText("");
+        txtCorreo.setText("");
+        txtContrasena.setText("");
+    }
+            
+    private void txtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNombreActionPerformed
+
+    private void txtContrasenaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtContrasenaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtContrasenaActionPerformed
+
+    private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
+        limpiarcampos();
+    }//GEN-LAST:event_btnLimpiarActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FormRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FormRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FormRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FormRegistro.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new FormRegistro().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JButton btnLimpiar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField txtContrasena;
+    private javax.swing.JTextField txtCorreo;
+    private javax.swing.JTextField txtNombre;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ CRUDUser204/src/UserCRUD.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author chant
+ */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class UserCRUD {
+    private Connection conexion;
+    
+    //constructor de la clase arranca la conexion a BD
+    public UserCRUD(){
+        conexion = ConexionMySQL.conectar();
+    }
+    
+    public boolean insertUsuario(String nombre, String correo, String contra){
+        
+        String insertSQL = "INSERT INTO usuarios(nombre,correo,contrasena) VALUES(?,?,?)";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(insertSQL);
+            ps.setString(1, nombre);
+            ps.setString(2, correo);
+            ps.setString(3, contra);
+            return ps.executeUpdate()>0;
+        }catch(SQLException e){
+            System.out.println("Error al crear el usuario "+ e.getMessage());
+            return false;
+        }
+    }//fin del insert
+    
+    //metodo para consultar por ID
+    public ResultSet bucarporID (int id){
+        String sqlBuscar = "SELECT * FROM usuarios WHERE id = ?";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sqlBuscar);
+            ps.setInt(1,id);
+            return ps.executeQuery();
+        }
+        catch(SQLException e){
+            System.out.println("Error al buscar por id "+ e.getMessage());
+            return null;
+        }
+    }//fin del consulta ID
+    
+    public ResultSet obtenerTodos(){
+        String slqTodos = "SELECT * FROM usuarios";
+        
+        try{
+            PreparedStatement ps = conexion.prepareStatement(slqTodos);
+            return ps.executeQuery();
+        }
+        catch(SQLException s){
+            System.out.println("Error al consultar todos"+s.getMessage());
+            return null;
+        }
+        
+    }//fin del obtener todos
+    
+    public boolean updateUsuario(String nombre, String correo, String contra, int id){
+        String sqlEditar = "UPDATE usuarios SET nombre = ?, correo = ?, contrasena = ? WHERE id = ?";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sqlEditar);
+            ps.setString(1, nombre);
+            ps.setString(2, correo);
+            ps.setString(3, contra);
+            ps.setInt(4, id);
+            return ps.executeUpdate()>0;
+        }catch(SQLException e){
+            System.out.println("Error al editar el usuario "+ e.getMessage());
+            return false;
+        }
+    }
+}//
